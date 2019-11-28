121989fd16da449f387ea2981279b6fe0e83a28f
Hoai Nam NGUYEN <hoainam.nguyen.ext@sodexo.com>
2019-11-28 12:00:19 +0100

    with diagonal

--- game.go
+++ game.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const (
 	SELF = iota
 	OPPONENT = iota
@@ -13,22 +15,77 @@ type gameState struct {
 
 type Board = [9]int
 
-func emptyBoard() *Board {
+func emptyBoard() Board {
 	var board [9]int
 	for i := 0; i < 9; i++ {
 		board[i] = EMPTY
 	}
-	return &board
+	return board
 }
 
 func checkRow(player int, row int, board *Board) bool {
+	for i:= row* 3; i < row * 3 + 3; i++ {
+		if board[i] != player {
+			return false
+		}
+	}
+	return true
+}
+
+func checkColumn(player int, column int, board *Board) bool {
+	for j:= column; j < 9; j += 3 {
+		if board[j] != player {
+			return false
+		}
+	}
+	return true
+}
+
+func checkFirstDiagonal(player int, board *Board) bool {
+	for i:=0; i<9; i +=4 {
+		if board[i] != player {
+			return false
+		}
+	}
+	return true
+}
+
+func checkSecondDiagonal(player int, board *Board) bool {
+	for i:=2; i <= 6; i+=2 {
+		if board[i] == player {
+			return false
+		}
+	}
+	return true
+}
+
+
+
+func checkWinner(player int, board *Board) bool {
+	for i := 0; i < 3; i++ {
+		if checkRow(player, i, board) || checkColumn(player, i, board) {
+			return true
+		}
+	}
 	return false
 }
 
-func findWinner(player int, board *Board) *int {
-	return nil
+func nextPlayer(player int) int {
+	switch player {
+	case SELF:
+		return OPPONENT
+	case OPPONENT:
+		return SELF
+	default:
+		panic(fmt.Sprintf("Player %v is not valid", player))
+	}
 }
 
-func get(i int, j int, board *Board) int {
-	return board[3 * i + j]
+func stateCopy(state gameState) gameState {
+	return gameState{state.self, state.board}
 }
+
+
+func nextPossibilities(state gameState) []gameState {
+ 	return []gameState{}
+ }
--- game_test.go
+++ game_test.go
@@ -2,10 +2,48 @@ package main
 
 import "testing"
 
-func TestGet(t *testing.T) {
-	board := emptyBoard()
-	first := get(0, 0, board)
-	if first != 2 {
-		t.Errorf("first value is incorrect: %d, want: %d.", first, 2)
+func TestCheckWinner(t *testing.T) {
+	board := [9]int{
+		EMPTY, EMPTY, EMPTY,
+		EMPTY, EMPTY, EMPTY,
+		EMPTY, EMPTY, EMPTY}
+	isSelfWinner := checkWinner(SELF, &board)
+	if isSelfWinner {
+		t.Errorf("Self is not winner")
+	}
+
+	isOpponentWinner := checkWinner(SELF, &board)
+	if isOpponentWinner {
+		t.Errorf("Opponent is not winner")
+	}
+}
+
+func TestCheckWinner1(t *testing.T) {
+	board := [9]int{
+		EMPTY, EMPTY, OPPONENT,
+		SELF, SELF, SELF,
+		EMPTY, EMPTY, OPPONENT}
+	isSelfWinner := checkWinner(SELF, &board)
+
+	if !isSelfWinner {
+		t.Errorf("Self must be winner")
+	}
+
+	isOpponentWinner := checkWinner(OPPONENT, &board)
+	if isOpponentWinner {
+		t.Errorf("Opponent is not winner")
+	}
+}
+
+func TestStateCopy(t *testing.T) {
+	board := [9]int{
+		EMPTY, EMPTY, OPPONENT,
+		SELF, SELF, SELF,
+		EMPTY, EMPTY, OPPONENT}
+	state := gameState{true, board}
+	copiedState := stateCopy(state)
+	state.board[0] = SELF
+	if copiedState.board[0] == SELF {
+		t.Errorf("Copied failed")
 	}
 }
